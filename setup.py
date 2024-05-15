9d51f69be7f5d064fe8ebb063473cd6c35cb7e83
Simon Lucey <slucey@Simons-MacBook-Pro-4.local>
2024-05-15 18:37:35 +0930

    Setup.py 2

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import torch
-
-# Image rescale and I/O
-from skimage import io
-from skimage.transform import rescale
-
-# stores different optimizors like SGD
-import torch.optim as optim
-
-# Some torch functions that are used multiple times
-import torch.nn.functional as F
-import torch.nn as nn
 
 # Set the class for Sinc activation
 class Sinc(nn.Module):
@@ -21,3 +8,4 @@ class Sinc(nn.Module):
 
     def forward(self, x):
         return torch.sinc(self.beta*x)
+    
